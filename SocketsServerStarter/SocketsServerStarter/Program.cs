f9c7df0cea9a5042cff32bd7e1af1fd923f06d05
Tiago Pompeo <tiapom@gmail.com>
2019-07-09 23:03:38 -0300

    FirsComitt

---
+++ SocketsServerStarter/SocketsServerStarter/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SocketsServerStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = IPAddress.Any;
+
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+
+            try
+            {
+                listenerSocket.Bind(ipep);
+
+                listenerSocket.Listen(5);
+
+                Console.WriteLine("About to accept incoming connection.");
+
+                Socket client = listenerSocket.Accept();
+
+                Console.WriteLine("Client connected. " + client.ToString() + " - IP End Point: " + client.RemoteEndPoint.ToString());
+
+                byte[] buff = new byte[128];
+
+                int numberOfReceivedBytes = 0;
+
+                while (true)
+                {
+
+                    numberOfReceivedBytes = client.Receive(buff);
+
+                    Console.WriteLine("Number of received bytes: " + numberOfReceivedBytes);
+
+                    Console.WriteLine("Data sent by client is: " + buff);
+
+                    string receivedText = Encoding.ASCII.GetString(buff, 0, numberOfReceivedBytes);
+
+                    Console.WriteLine("Data sent by client is: " + receivedText);
+
+                    client.Send(buff);
+
+                    if (receivedText == "x")
+                    {
+                        break;
+                    }
+
+                    Array.Clear(buff, 0, buff.Length);
+                    numberOfReceivedBytes = 0;
+                }
+            }
+            catch(Exception excp)
+            {
+                Console.WriteLine(excp.ToString());
+            }
+            
+        }
+    }
+}
